a5c55e5a2ac3c0371e42a187a17c3b16593d9b4d
CHRIS <CHRIS@192.168.1.10>
2016-04-11 23:15:07 +0200

    Small edits in files

--- FlowSolid 2/src/controller/ControlQuiz.java
+++ FlowSolid 2/src/controller/ControlQuiz.java
@@ -3,55 +3,51 @@ package controller;
 import controller.ControlPairs;
 import controller.interfaces.QuizControlInterface;
 
-
-
 public class ControlQuiz extends ControlPairs implements QuizControlInterface{
+    
     /**
-* This method returns a list of selectable games.
-* Pre: 
-* Post: 
-* Returns a list of names of selectable games 
-*/
-public String[] getGameNames(){
-    String strAr[] = {""}; // TEMP
-    
-    return strAr;
-}
+    * This method returns a list of selectable games.
+    * Pre: 
+    * Post: 
+    * Returns a list of names of selectable games 
+    */
+    public String[] getGameNames() {
+        String strAr[] = {""}; // TEMP
+        return strAr;
+    }
 
 
-/**
-* This method is used to select a game and mark it as the selected one. 
-* It should be called when the user has made a choise
-* about what game he wants.
-* Pre: The name passed corresponds to a selectable game
-* Post: The existing collection of word pairs is cleared.
-*/
-public void selectGame(String gameName){
-    
-}
+    /**
+    * This method is used to select a game and mark it as the selected one. 
+    * It should be called when the user has made a choise
+    * about what game he wants.
+    * Pre: The name passed corresponds to a selectable game
+    * Post: The existing collection of word pairs is cleared.
+    */
+    public void selectGame(String gameName) {
+        
+    }
 
 
-/**
-* This method returns the name of the currently selected game.
-* Pre: 
-* Post: Returns the name of the game presently selected. If no game is selected it returns null.
-*/
-public String getSelectedGameName(){
-    String name = "";
-    
-    return name;
-}
+    /**
+    * This method returns the name of the currently selected game.
+    * Pre: 
+    * Post: Returns the name of the game presently selected. If no game is selected it returns null.
+    */
+    public String getSelectedGameName() {
+        String name = "";
+        
+        return name;
+    }
 
 
-  /**
-   * This method must add a new game to the existing collection of games.
-   * Pre: 
-   * Post: A new game is added to the existing collection of games
-   */
-public void addGame(String gameName){
-    
-}
-
-
+      /**
+       * This method must add a new game to the existing collection of games.
+       * Pre: 
+       * Post: A new game is added to the existing collection of games
+       */
+    public void addGame(String gameName) {
+        
+    }
 
 }
--- FlowSolid 2/src/controller/interfaces/QuizControlInterface.java
+++ FlowSolid 2/src/controller/interfaces/QuizControlInterface.java
@@ -1,47 +1,41 @@
 package controller.interfaces;
 
+public interface QuizControlInterface extends WordPairControlInterface {
+    // rev chu 18-04-2013
+    // rev pelo 25-11-2013, 29-03-2014
 
-public interface QuizControlInterface extends WordPairControlInterface
-{
-// rev chu 18-04-2013
-// rev pelo 25-11-2013, 29-03-2014
-
-
-
-/**
-* This method returns a list of selectable games.
-* Pre: 
-* Post: 
-* Returns a list of names of selectable games 
-*/
-String[] getGameNames();
+    /**
+    * This method returns a list of selectable games.
+    * Pre: 
+    * Post: 
+    * Returns a list of names of selectable games 
+    */
+    String[] getGameNames();
 
 
-/**
-* This method is used to select a game and mark it as the selected one. 
-* It should be called when the user has made a choise
-* about what game he wants.
-* Pre: The name passed corresponds to a selectable game
-* Post: The existing collection of word pairs is cleared.
-*/
-void selectGame(String gameName);
+    /**
+    * This method is used to select a game and mark it as the selected one. 
+    * It should be called when the user has made a choice
+    * about what game he wants.
+    * Pre: The name passed corresponds to a selectable game
+    * Post: The existing collection of word pairs is cleared.
+    */
+    void selectGame(String gameName);
 
 
-/**
-* This method returns the name of the currently selected game.
-* Pre: 
-* Post: Returns the name of the game presently selected. If no game is selected it returns null.
-*/
-String getSelectedGameName();
+    /**
+    * This method returns the name of the currently selected game.
+    * Pre: 
+    * Post: Returns the name of the game presently selected. If no game is selected it returns null.
+    */
+    String getSelectedGameName();
 
 
-  /**
-   * This method must add a new game to the existing collection of games.
-   * Pre: 
-   * Post: A new game is added to the existing collection of games
-   */
-void addGame(String gameName);
-
+      /**
+       * This method must add a new game to the existing collection of games.
+       * Pre: 
+       * Post: A new game is added to the existing collection of games
+       */
+    void addGame(String gameName);
 
 }
-
